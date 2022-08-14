449ab3c61c40331be80da8f407ecd615b4676f61
Benjamin Masor <benjaminmasor@gmail.com>
2022-08-15 01:37:09 +1000

    fixed: cors issue & added: relation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Res,
+  HttpStatus,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 import { AuthService } from './auth.service';
 import { AuthCredentials } from './dto/authCredentials.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -17,5 +27,11 @@ export class AuthController {
   async register(@Body() credentials: AuthCredentials, @Res() res: Response) {
     const user = await this.authService.register(credentials);
     return res.status(HttpStatus.CREATED).json(user);
+  }
+
+  @Get('profile')
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Req() req: Request, @Res() res: Response) {
+    return res.status(HttpStatus.ACCEPTED).json(req.user);
   }
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -22,6 +22,6 @@ export class User {
   })
   status: string;
 
-  @OneToMany(() => Post, (post) => post.user)
+  @OneToMany(() => Post, (post) => post.user, { cascade: true })
   posts: Post[];
 }
